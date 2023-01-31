522d4e40897e94f5ba2393fedc9f6e900d72124b
thomas <tom.angotti11@gmail.com>
2023-01-31 14:46:58 -0700

    commit tree b17501ab3cd13ef14ad1605605be1b4ae9debb8e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[., !, ?]/).count
   end
 end
+
+
+puts "Hello world.".sentence?
+puts "Am I a question?".question?
+puts "LETS GET EXCITED!".exclamation?
+puts "AM I multple. or single? also hi!".count_sentences
